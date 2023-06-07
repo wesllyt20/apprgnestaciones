ba250eda16642647b14b03c3d994743176cdc476
wesllyt20 <raphael.09@hotmail.es>
2023-06-07 17:28:38 -0500

    first commit

---
+++ src/components/contFuncion.vue
@@ -0,0 +1,248 @@
+<template>
+  <div>
+    <h2 id="tiFunc">Guía de uso</h2>
+    <p class="descrFunc">Funcionalidades de los iconos</p>
+    <br />
+
+    <div class="contFuncion">
+      <div class="svgImg">
+        <svg fill="#0000AF" width="40px" height="40px" viewBox="-4.08 -4.08 32.16 32.16" xmlns="http://www.w3.org/2000/svg" id="filter" class="icon glyph" stroke="#0000AF"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M20.62,3.17A2,2,0,0,0,18.8,2H5.2A2,2,0,0,0,3.7,5.32L9,11.38V21a1,1,0,0,0,.47.85A1,1,0,0,0,10,22a1,1,0,0,0,.45-.11l4-2A1,1,0,0,0,15,19V11.38l5.3-6.06A2,2,0,0,0,20.62,3.17Z"></path></g></svg>
+      </div>
+
+      <p class="parrafoFun">
+        <span class="pasos"> Filtro : </span>
+        Selecciona el departamento y ciudad según la lista.
+      </p>
+    </div>
+    <br />
+    <div class="contFuncion">
+      <div class="svgImg">
+        <svg
+          width="40px"
+          height="40px"
+          viewBox="-1.92 -1.92 27.84 27.84"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M11.553 1.10557C11.8346 0.964809 12.1659 0.964809 12.4475 1.10557L22.4475 6.10557C22.7862 6.27496 23.0002 6.62123 23.0002 7C23.0002 7.37877 22.7862 7.72504 22.4475 7.89443L12.4475 12.8944C12.1659 13.0352 11.8346 13.0352 11.553 12.8944L1.55303 7.89443C1.21425 7.72504 1.00024 7.37877 1.00024 7C1.00024 6.62123 1.21425 6.27496 1.55303 6.10557L11.553 1.10557ZM4.23631 7L12.0002 10.882L19.7642 7L12.0002 3.11803L4.23631 7Z"
+              fill="#0000AF"
+            ></path>
+            <path
+              d="M1.10579 16.5528C1.35278 16.0588 1.95345 15.8586 2.44743 16.1055L12.0002 20.8819L21.553 16.1055C22.047 15.8586 22.6477 16.0588 22.8946 16.5528C23.1416 17.0467 22.9414 17.6474 22.4474 17.8944L12.4474 22.8944C12.1659 23.0352 11.8345 23.0352 11.553 22.8944L1.55301 17.8944C1.05903 17.6474 0.858803 17.0467 1.10579 16.5528Z"
+              fill="#0000AF"
+            ></path>
+            <path
+              d="M2.44743 11.1055C1.95345 10.8586 1.35278 11.0588 1.10579 11.5528C0.858803 12.0467 1.05903 12.6474 1.55301 12.8944L11.553 17.8944C11.8345 18.0352 12.1659 18.0352 12.4474 17.8944L22.4474 12.8944C22.9414 12.6474 23.1416 12.0467 22.8946 11.5528C22.6477 11.0588 22.047 10.8586 21.553 11.1055L12.0002 15.8819L2.44743 11.1055Z"
+              fill="#0000AF"
+            ></path>
+          </g>
+        </svg>
+      </div>
+
+      <p class="parrafoFun">
+        <span class="pasos"> Capas : </span>
+        Selecciona el departamento y ciudad para acceder a la información
+        disponible.
+      </p>
+    </div>
+    <br />
+    <div class="contFuncion">
+      <div class="svgImg">
+        <svg
+          width="40px"
+          height="40px"
+          viewBox="-3 -3 26.00 26.00"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="#FFFFFF"
+        >
+          <g
+            id="SVGRepo_bgCarrier"
+            stroke-width="0"
+            transform="translate(1.4000000000000004,1.4000000000000004), scale(0.86)"
+          >
+            <rect
+              x="-3"
+              y="-3"
+              width="26.00"
+              height="26.00"
+              rx="4.16"
+              fill="#0000AF"
+              strokewidth="0"
+            ></rect>
+          </g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              stroke="#FFFFFF"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M17 5L8 15l-5-4"
+            ></path>
+          </g>
+        </svg>
+      </div>
+
+      <p class="parrafoFun">
+        <span class="pasos"> Check : </span>
+        Utiliza esta función sobre el tipo de estudio para acceder a la
+        información.
+      </p>
+    </div>
+    <br />
+    <div class="contFuncion">
+      <div class="svgImg">
+        <svg
+          width="40px"
+          height="40px"
+          viewBox="0 0 64.00 64.00"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke-width="2.176"
+          stroke="#0000AF"
+          fill="none"
+          transform="matrix(-1, 0, 0, 1, 0, 0)"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <line x1="50.69" y1="32" x2="56.32" y2="32"></line>
+            <line x1="7.68" y1="32" x2="38.69" y2="32"></line>
+            <line x1="26.54" y1="15.97" x2="56.32" y2="15.97"></line>
+            <line x1="7.68" y1="15.97" x2="14.56" y2="15.97"></line>
+            <line x1="35" y1="48.03" x2="56.32" y2="48.03"></line>
+            <line x1="7.68" y1="48.03" x2="23" y2="48.03"></line>
+            <circle cx="20.55" cy="15.66" r="6"></circle>
+            <circle cx="44.69" cy="32" r="6"></circle>
+            <circle cx="29" cy="48.03" r="6"></circle>
+          </g>
+        </svg>
+      </div>
+
+      <p class="parrafoFun">
+        <span class="pasos">Leyenda : </span>
+        Utiliza esta función para visualizar mayor información.
+      </p>
+    </div>
+
+    <br />
+   
+
+    <br />
+
+    
+    <a id="btnAcercade" class="my-linktop" @click="recepGeo">Ver Acerca de</a>
+    <br />
+    <br />
+  </div>
+</template>
+  
+<script>
+export default {
+  methods: {
+    recepGeo() {
+      // Lógica de la función receptora
+      document.querySelectorAll("#acerca").forEach((boton) => boton.click());
+    },
+  },
+};
+</script>
+  
+  <style>
+#tiFunc {
+  font-style: normal;
+  font-size: 20px;
+  line-height: 26px;
+  color: #0a1fc2;
+  margin: 45px 0px 12px 45px;
+}
+.descrFunc {
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 22px;
+  color: #00132e;
+  margin: 0px 15px 0px 45px;
+}
+.parrafoFun {
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 22px;
+  color: #00132e;
+  margin: 0px 15px 0px 25px;
+}
+.pasos {
+  color: #0000af;
+  font-weight: 600;
+}
+.miniIcons {
+  width: 20px;
+  height: 20px;
+}
+.svgImg {
+  margin: 0px 0px 5px 44px;
+  border-radius: 3.57865px;
+  box-shadow: 3px 5px 5px 2px rgba(0, 0, 0, 0.5);
+  width: 32px;
+  height: 32px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+}
+.contFuncion {
+  display: flex;
+  align-items: start;
+}
+
+.cirNum {
+  text-align: center;
+  line-height: 26.33px;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 21px;
+  margin-top: 2px;
+}
+.anuncio {
+  margin: 0px 30px 0px 45px;
+}
+#btnAcercade {
+  width: 195px;
+  height: 41px;
+  background: #ffffff;
+  border: 1px solid #0032FF;
+  color: #0032FF;
+  border-radius: 4.26437px;
+  cursor: pointer;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 45px;
+}
+#btnAcercade:hover {
+  background-color: #0000af;
+  color:#ffffff;
+}
+</style>
---
+++ src/components/contGeofisico.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <div class="bg-image2" />
+    <h2 id="tiGeo" class="">Acerca de</h2>
+    <br />
+    <div id="parGeo" class="mb-5">
+      <!-- <p class="parrafoGeo">
+        Las primeras estaciones acelerométricas (acelerógrafos) a cargo del
+        Instituto Geofísico del Perú, funcionaron en los años 40 en el Parque de
+        la Reserva y en el campus de la Universidad de La Molina, esta última
+        aún operativa, pero con instrumental moderno. En la década de los 80 y
+        90, la red acelerométrica estuvo integrada por 17 estaciones , todas con
+        registros en papel fotográfico y/o cintas magnéticas. 
+      </p>-->
+      <p class="parrafoGeo pb-8">
+        Red Geofísica Nacional (REDGEN) es la red administrada por el IGP que
+        permite el monitoreo y vigilancia de los peligros de origen natural y
+        antrópicos asociados con la dinámica interna y externa de la Tierra,
+        constituyendo la fuente de información oficial del Estado sobre este
+        tipo de peligros para la gestión del riesgo de desastres.
+      </p>
+      <!--<a id="btnFunci" class="my-linktop" @click="recepFunc">Ver Guia de uso</a>-->
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    recepFunc() {
+      document.querySelectorAll("#func").forEach((boton) => boton.click());
+    },
+    recepContac() {
+      document.querySelectorAll("#contacto").forEach((boton) => boton.click());
+    },
+  },
+};
+</script>
+
+<style>
+#tiGeo {
+  font-style: normal;
+  font-size: 20px;
+  line-height: 26px;
+  color: #0000Af;
+  margin: 0px 0px 0px 45px;
+}
+#parGeo {
+  margin-bottom: 80px !important;
+}
+.parrafoGeo {
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 24px;
+  color: #00132e;
+  margin-left: 45px;
+  margin-right: 45px;
+}
+
+.bg-image2 {
+  width: 450px;
+  height: 370px;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0) 85%,
+      rgba(255, 255, 255, 1) 97%
+    ),
+    url("https://ide.igp.gob.pe/geovisor/redgen/imagenEstacion.png");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-attachment: fixed;
+}
+
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #bbbbbb;
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #888;
+}
+#btnFunci {
+  width: 195px;
+  height: 41px;
+  background: #ffffff;
+  border: 1px solid #0000Af;
+  color: #0000Af;
+  border-radius: 4.26437px;
+  cursor: pointer;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 45px;
+}
+#btnFunci:hover {
+  background-color: #0000af;
+  color: #ffffff;
+}
+</style>
